f4a35af909cc3d4c7bb0411a0bb54243bf517821
Kami <mihailopetrovic995@gmail.com>
2020-04-09 15:39:05 +0200

    Updates

---
+++ precache-manifest.4fac86707f8b8965c284d4ca2e8a065e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbd160b39b8ac0337a4665d69da76ab",
+    "url": "/drum/index.html"
+  },
+  {
+    "revision": "b7a978236252f73738bd",
+    "url": "/drum/static/css/main.4a309ed6.chunk.css"
+  },
+  {
+    "revision": "dc4c28afddc899960541",
+    "url": "/drum/static/js/2.5710bb76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum/static/js/2.5710bb76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a978236252f73738bd",
+    "url": "/drum/static/js/main.ef8eb916.chunk.js"
+  },
+  {
+    "revision": "16daf70b20f07530e517",
+    "url": "/drum/static/js/runtime-main.290ec04f.js"
+  }
+]);
